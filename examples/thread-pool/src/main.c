8edcc7150e2655658b7846a268ca249cc37c6305
moriar1 <reflowcion@tuta.io>
2025-12-14 00:18:32 +0500

    Add threads in tread-pool, but without wait and shutdown.

--- examples/thread-pool/src/main.c
+++ examples/thread-pool/src/main.c
@@ -1,11 +1,16 @@
 #include <limits.h>
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "custom.h" // DEBUG_PUTS, safe MALLOC, etc.
 
+enum { NUM_THREADS = 6 };
+
 ///
 void *mystruct_sum_print(void *);
+void *threadpool_thread_start(void *);
 typedef struct {
   int a;
   int b;
@@ -23,25 +28,52 @@ struct Task {
 typedef struct {
   Task *head;
   Task *tail;
-  size_t size;
+  size_t task_count;
+  // size_t task_count;
   pthread_mutex_t mutex;
-} TaskQueue;
+  pthread_cond_t cond_idle;
+  bool shutdown;
+} ThreadPool;
 
-static int queue_init(TaskQueue *queue) {
-  if (queue == NULL) {
-    DEBUG_PUTS("queue_init: queue is NULL");
-    return -1;
+static ThreadPool *queue_init(unsigned nthreads) {
+  ThreadPool *pool = CALLOC(1, sizeof(*pool));
+
+  // `Threads`
+  // #pragma unroll
+  for (unsigned i = 0; i < nthreads; i++) {
+    pthread_t thread;
+    if (pthread_create(&thread, NULL, threadpool_thread_start, pool) != 0) {
+      DEBUG_PUTS("err: pcreate");
+      return NULL;
+    }
+    // Maybe no need in err checking
+    if (pthread_detach(thread)) {
+      DEBUG_PUTS("err: pdetatch");
+      return NULL;
+    }
   }
-  queue->head = NULL;
-  queue->tail = NULL;
-  queue->size = 0;
+  if (pthread_mutex_init(&pool->mutex, NULL) != 0) {
+    DEBUG_PUTS("err: mutex_init");
+    return NULL;
+  }
+  if (pthread_cond_init(&pool->cond_idle, NULL) != 0) {
+    DEBUG_PUTS("err: cond_init");
+    if (pthread_mutex_destroy(&pool->mutex) != 0) {
+      DEBUG_PUTS("err: mutex_destroy");
+    }
+    return NULL;
+  }
+  pool->shutdown = false;
 
-  pthread_mutex_init(&queue->mutex, NULL);
+  // `Queue`
+  pool->head = NULL;
+  pool->tail = NULL;
+  pool->task_count = 0;
 
-  return 0;
+  return pool;
 }
 
-static int queue_destroy(TaskQueue *queue) {
+static int threadpool_destroy(ThreadPool *queue) {
   if (queue == NULL) {
     DEBUG_PUTS("queue is NULL, nothing to free");
     return -1;
@@ -58,7 +90,8 @@ static int queue_destroy(TaskQueue *queue) {
 
   queue->head = NULL;
   queue->tail = NULL;
-  queue->size = 0;
+  queue->task_count = 0;
+  queue->shutdown = false;
   if (pthread_mutex_destroy(&queue->mutex) != 0) {
     DEBUG_PUTS("mdestroy");
     return -1;
@@ -66,8 +99,8 @@ static int queue_destroy(TaskQueue *queue) {
   return 0;
 }
 
-static int queue_push(TaskQueue *queue, void *(*func)(void *), void *arg) {
-  if (queue == NULL) {
+static int queue_push(ThreadPool *pool, void *(*func)(void *), void *arg) {
+  if (pool == NULL) {
     DEBUG_PUTS("queue is NULL, cannot push");
     return -1;
   }
@@ -77,20 +110,21 @@ static int queue_push(TaskQueue *queue, void *(*func)(void *), void *arg) {
   new_node->func = func;
   new_node->arg = arg;
 
-  pthread_mutex_lock(&queue->mutex); // or move lock at the begining
-  queue->size++;
-  if (queue->head == NULL) { // empty queue
-    queue->head = new_node;
-    queue->tail = new_node;
+  pthread_mutex_lock(&pool->mutex); // or move lock at the begining
+  pool->task_count++;
+  if (pool->head == NULL) { // empty queue
+    pool->head = new_node;
+    pool->tail = new_node;
   } else {
-    queue->tail->next = new_node; // not empty queue
-    queue->tail = new_node;
+    pool->tail->next = new_node; // not empty queue
+    pool->tail = new_node;
   }
-  pthread_mutex_unlock(&queue->mutex);
+  pthread_cond_signal(&pool->cond_idle);
+  pthread_mutex_unlock(&pool->mutex);
   return 0;
 }
 
-static int queue_pop(TaskQueue *queue, Task **task_ptr) {
+static int queue_pop(ThreadPool *queue, Task **task_ptr) {
   if (queue == NULL) {
     DEBUG_PUTS("queue is NULL, nothing to pop");
     return -1;
@@ -110,33 +144,62 @@ static int queue_pop(TaskQueue *queue, Task **task_ptr) {
   if (queue->head == NULL) {
     queue->tail = NULL;
   }
-  queue->size--;
+  queue->task_count--;
 
   pthread_mutex_unlock(&queue->mutex);
   return 0;
 }
-
+///
 void *mystruct_sum_print(void *arg) {
   MyStruct *m = arg;
 
   int sum = m->a + m->b;
-  printf("%s, %d+%d=%d\n", m->msg, m->a, m->b, sum);
+  pthread_t self = pthread_self();
+  printf("%lu: %s, %d+%d=%d\n", (unsigned long)self, m->msg, m->a, m->b, sum);
   m->msg[1] = 'X';
   return m->msg;
 }
 
+void *threadpool_thread_start(void *arg) {
+  ThreadPool *pool = arg;
+
+  while (true) {
+    pthread_mutex_lock(&pool->mutex);
+    while (pool->task_count == 0 && !pool->shutdown) {
+      pthread_cond_wait(&pool->cond_idle, &pool->mutex);
+    }
+    if (pool->shutdown) { // FIXME: incomplete
+      break;
+    }
+
+    pthread_mutex_unlock(&pool->mutex);
+    Task *task;
+    if (queue_pop(pool, &task) == 0) { // thread safe pop
+      char *ptr = task->func(task->arg);
+      puts(ptr);
+      FREE(task);
+    }
+  }
+  pthread_mutex_unlock(&pool->mutex);
+  return (void *)0;
+}
+/// end
+
 int main(void) {
-  TaskQueue queue;
-  queue_init(&queue);
+  ThreadPool *thread_pool = queue_init(NUM_THREADS);
+  if (thread_pool == NULL) {
+    perror("thread_pool is NULL");
+    return 1;
+  }
 
   char msg[] = "Hello";
-  MyStruct mystruct = {1, 2, msg};
-  queue_push(&queue, mystruct_sum_print, &mystruct);
-  Task *pop_task;
-  if (queue_pop(&queue, &pop_task) == 0) {
-    char *ptr = pop_task->func(pop_task->arg);
-    puts(ptr);
-    FREE(pop_task);
-  }
-  queue_destroy(&queue);
+  MyStruct mystruct1 = {1, 2, msg};
+  queue_push(thread_pool, mystruct_sum_print, &mystruct1);
+  MyStruct mystruct = {2, 3, msg};
+  queue_push(thread_pool, mystruct_sum_print, &mystruct);
+  sleep(1);
+  threadpool_destroy(thread_pool);
 }
+
+// TODO: threadpool_wait, shutdown
+// pthread_cond_broadcast(&thread_pool->cond_shutdown);
